7f6292f9c1f322ce8979cccbb296b72b611f3ee2
Aanand Mahawal <151204835+aanandmahawal@users.noreply.github.com>
2026-09-03 18:01:20 +0530

    Update About.js

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import Jump from "react-reveal/Jump";
-import profilePic from "../../assets/images/profile.jpg";
+// import profilePic from "../../assets/images/profile.jpg";
 const About = () => {
   return (
     <>
