7f5596a914c4c20b8d20d1cb6b4effc8be221335
Aanand Mahawal <151204835+aanandmahawal@users.noreply.github.com>
2026-09-04 11:47:50 +0530

    Update Layout.js

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Home from "../../pages/Home/Home";
 import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
 import "./Layout.css";
 import Menus from "../Menus/Menus";
+import ParticleBackground from "./ParticleBackground";
 
 const Layout = () => {
   const [toggle, setToggle] = useState(true);
@@ -14,6 +15,7 @@ const Layout = () => {
   return (
     <>
       <div className="sidebar-section">
+        <ParticleBackground />
         <div className={toggle ? "sidebar-toggle sidebar" : "sidebar"}>
           <div className="sidebar-toggle-icons">
             <p onClick={handleToggle}>
